02081a92afe33afccb856389f3a53ab02b4c31e9
lvazquezdorna <lvazquezdorna@nautilus17.danielcastelao.org>
2018-10-09 09:30:11 +0200

    subiendo desde netbeans

---
+++ Boletin.1_1/src/boletin/pkg1_1/Boletin1_1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin.pkg1_1;
+
+/**
+ * calcular el area de un triangulo
+ * @author lvazquezdorna
+ */
+public class Boletin1_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       //variables
+       int base=4, altura= 3;
+       
+        System.out.println("El triangulo con base "+base+ " y altura "+altura+  " tiene un area de "+(base*altura/2));
+        
+        
+        
+        
+    }
+    
+}
