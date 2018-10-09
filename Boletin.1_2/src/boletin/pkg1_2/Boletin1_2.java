ee652d61007d18793068be94a180103e5aa3dc6e
lvazquezdorna <lvazquezdorna@nautilus17.danielcastelao.org>
2018-10-09 09:30:16 +0200

    subiendo desde netbeans

---
+++ Boletin.1_2/src/boletin/pkg1_2/Boletin1_2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin.pkg1_2;
+
+/**
+ *calcular el area de un cuadrado
+ * @author lvazquezdorna
+ */
+public class Boletin1_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // variable
+        int lado=3;
+        System.out.println("El area del cuadrado con lado 3 es "+(lado*lado));
+        
+        
+    }
+    
+}
